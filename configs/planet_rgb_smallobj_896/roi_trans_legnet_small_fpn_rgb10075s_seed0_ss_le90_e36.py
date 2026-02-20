73cb2682cdaf51550b45bd2ec1d938db1e76a5e7
someuser2026 <forgithub2026@gmail.com>
2026-02-20 15:32:06 +1100

    Update rgb10075 configs

--- configs/planet_rgb_smallobj_896/roi_trans_legnet_small_fpn_rgb10075s_seed0_ss_le90_e36.py
+++ configs/planet_rgb_smallobj_896/roi_trans_legnet_small_fpn_rgb10075s_seed0_ss_le90_e36.py
@@ -187,13 +187,6 @@ train_pipeline = [
         flip_ratio=[0.25, 0.25, 0.25],
         direction=['horizontal', 'vertical', 'diagonal'],
         version=angle_version),
-    dict(
-        type='PolyRandomRotate',
-        rotate_ratio=0.5,
-        angles_range=180,
-        auto_bound=False,
-        rect_classes=[9, 11],
-        version=angle_version),
     dict(type='Normalize', **img_norm_cfg),
     dict(type='Pad', size_divisor=32),
     dict(type='DefaultFormatBundle'),
